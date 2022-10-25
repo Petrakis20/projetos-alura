b8a1fd372b633aa423c3b4119a96906d2872ec1b
Your Name <you@example.com>
2022-10-24 22:57:16 -0300

    24/10

--- FRONT-END/aluramidi-curso-arquivos-iniciais/main.js
+++ FRONT-END/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,12 @@
-// POM -------------------------------
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play()    
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        alert('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -10,8 +16,18 @@ for (let i = 0; i < listaDeTeclas.length; i++) {
     const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
-    listaDeTeclas[i].onclick = function (){
+
+    tecla.onclick = function() {
         tocaSom(idAudio);
     };
 
+    tecla.onkeydown = function(evento) {
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    };
+    
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    };
 }
